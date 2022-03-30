749b584d591de0359e69bc2ceb761832566861fe
Abishek Bhandari <52709870+Avisheikh@users.noreply.github.com>
2022-03-30 17:38:17 +0545

    created register table.

---
+++ register/src/main/java/com/practise/register/RegisterApplication.java
@@ -0,0 +1,15 @@
+package com.practise.register;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RegisterApplication {
+
+	public static void main(String[] args)
+	{
+		SpringApplication.run(RegisterApplication.class, args);
+		System.out.println();
+	}
+
+}
