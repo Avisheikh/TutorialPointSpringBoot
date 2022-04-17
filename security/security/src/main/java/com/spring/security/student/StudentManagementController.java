f1ed62e17e04ff098cc9a8d3759e4d0dc22bed9d
Abishek Bhandari <52709870+Avisheikh@users.noreply.github.com>
2022-04-17 22:28:32 +0545

    Update StudentManagementController.java

--- security/security/src/main/java/com/spring/security/student/StudentManagementController.java
+++ security/security/src/main/java/com/spring/security/student/StudentManagementController.java
@@ -1,5 +1,6 @@
 package com.spring.security.student;
 
+import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.Arrays;
@@ -17,24 +18,28 @@ public class StudentManagementController
     );
 
     @GetMapping
+    @PreAuthorize("hasAnyRole('ROLE_ADMIN', 'ROLE_ADMINTRAINEE')")
     public List<Student> getAllStudents()
     {
         return students;
     }
 
     @PostMapping
+    @PreAuthorize("hasAuthority('student:write')")
     public void registerNewStudent(@RequestBody Student student)
     {
         System.out.println(student);
     }
 
     @DeleteMapping(path = "{studentId}")
+    @PreAuthorize("hasAuthority('student:write')")
     public void deleteStudent(@PathVariable("studentId") Integer studentID)
     {
         System.out.println(studentID);
     }
 
     @PutMapping(path = "{studentId}")
+    @PreAuthorize("hasAuthority('student:write')")
     public void updateStudent(Integer studentId, Student student)
     {
         System.out.println(String.format("%s %s", studentId, student));
