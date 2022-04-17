5bcc765a4c9e478f8ec7185f5a84efc222b5c906
Abishek Bhandari <52709870+Avisheikh@users.noreply.github.com>
2022-04-17 22:28:27 +0545

    role and permission based authentication let knowdeldge

--- security/security/src/main/java/Enum/Test2.java
+++ security/security/src/main/java/Enum/Test2.java
@@ -1,28 +1,28 @@
 package Enum;
 
-enum Color2{
-    RED,
-    GREEN,
-    BLUE;
-    
-    // enum constructor called separately for each
-    private Color2()
-    {
-        System.out.println("Constructor called for : " + this.toString());
-    }
-    
-    public void colorInfo()
-    {
-        System.out.println("Universal Color");
-    }
-    
-}
+//enum Color{
+//    RED,
+//    GREEN,
+//    BLUE;
+//
+//    // enum constructor called separately for each
+//    private Color()
+//    {
+//        System.out.println("Constructor called for : " + this.toString());
+//    }
+//
+//    public void colorInfo()
+//    {
+//        System.out.println("Universal Color");
+//    }
+//
+//}
 
 public class Test2 
 {
-    public static void main(String[] args) {
-        Color2 c1 = Color2.RED;
-        System.out.println(c1);
-        c1.colorInfo();
-    }
+//    public static void main(String[] args) {
+//        Color c1 = Color.RED;
+//        System.out.println(c1);
+//        c1.colorInfo();
+//    }
 }
