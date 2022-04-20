de23414997be8bd1ebe9c10879b2c4169c286ca6
Abishek Bhandari <52709870+Avisheikh@users.noreply.github.com>
2022-04-20 15:19:51 +0545

    jwt new file created

---
+++ security/security/src/main/java/com/spring/security/Repo/UserRepo.java
@@ -0,0 +1,12 @@
+package com.spring.security.Repo;
+
+import com.spring.security.entity.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface UserRepo extends JpaRepository<User, Long> {
+
+    User findByUsername(String username);
+
+}
